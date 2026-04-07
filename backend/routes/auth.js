af0ad8a7e90048f8d06a0b8368863c5b05da5d9e
CalvinTyminski <calvintyminski@hotmail.com>
2026-04-06 21:13:36 -0600

    Added authorization with register and login with JWT tokens

---
+++ backend/routes/auth.js
@@ -0,0 +1,52 @@
+const express = require('express'); 
+const router = express.Router(); 
+
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+router.post('/register', async (req, res) => {
+    try{
+        const {username, password, role} = req.body; 
+
+        const existingUser = await User.findOne({username});
+        if (existingUser) {
+            return res.status(400).json({message: "User already exists"});
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new User({
+            username, password: hashedPassword, role
+        }); 
+
+        await user.save(); 
+
+        res.json({message: "User Registered successfully"});
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+});
+
+router.post('/login', async(req, res) =>{
+    try{
+        const { username, password } = req.body; 
+
+        const user = await User.findOne({username}); 
+        if(!user){
+            return res.status(400).json({message: "Invalid password"});
+        }
+
+        const token = kwt.sign(
+            { id: user._id, role: user.role},
+            process.env.JWT_SECRET,
+            { expiresIn: "1h"}
+        );
+
+        res.json({token}); 
+    } catch (error){
+        res.status(500).json({error: error.message});
+    }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,23 @@ require ('dotenv').config();
 const connectDB = require('./config/db')
 const express = require('express');
 const cors = require('cors');
+const authRoutes = require('./routes/auth');
+const authMiddleware = require('./middleware/authMiddleware');
 
 const app = express();
 
 connectDB();
 
+app.use('/api/auth', authRoutes);
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('API running...');
+});
+
+app.get('/api/test', authMiddleware, (req, res) =>{
+    res.json({message: "Protected route working", user: req.user});
 });
 
 const PORT = process.env.PORT || 5000;
