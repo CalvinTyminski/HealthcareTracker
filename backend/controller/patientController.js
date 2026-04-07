de7d4d6890b60f813b4824d2567d01345b56bbcd
isaac-adesanya <adesanyatosin07@gmail.com>
2026-04-07 17:15:37 -0600

    added methods to get all patients and get a patient by Id

--- backend/controller/patientController.js
+++ backend/controller/patientController.js
@@ -8,3 +8,23 @@ const createPatient = async (req, res) => {
         res.status(500).json({message: err.message});
     }
 };
+
+const getAllPatients = async (req, res) => {
+    try {
+        const patients = await Patient.find;
+        res.json(patients)
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+};
+
+const getPatientById = async (req, res) => {
+    try {
+        const patient = await Patient.findById(req.params.id);
+        if (!patient) return res.status(404).json({message: "Not found"})
+        res.status(201).json(patient)
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+};
+
