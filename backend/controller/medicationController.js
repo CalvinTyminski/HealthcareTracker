c271f47d51952c302332b0bfb8b2fd281ff802fd
isaac-adesanya <adesanyatosin07@gmail.com>
2026-04-07 17:39:03 -0600

    added a method to get all medications by patient

--- backend/controller/medicationController.js
+++ backend/controller/medicationController.js
@@ -1,6 +1,6 @@
 const Medication = require("../models/Medication");
 
-createMedication = async (req, res) => {
+const createMedication = async (req, res) => {
     try {
         const medication = await Medication.create({
             ...req.body,
@@ -8,6 +8,18 @@ createMedication = async (req, res) => {
         });
 
         res.status(201).json(medication);
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+};
+
+const getMedicationsByPatient = async (req, res) => {
+    try {
+        const meds = await Medication.find({
+            patientId: req.params.patientId
+        });
+
+        res.status(201).json(meds);
     } catch (err) {
         res.status(500).json({message: err.message});
     }
