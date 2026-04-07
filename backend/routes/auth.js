04440f03aec23ee4e23d140937d4f4182eee6cdc
isaac-adesanya <adesanyatosin07@gmail.com>
2026-04-07 16:23:33 -0600

    redoing because i pushed without saving the code

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,52 +1,8 @@
 const express = require('express'); 
 const router = express.Router(); 
+const {register, login} = require('../controller/userController')
 
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-
-router.post('/register', async (req, res) => {
-    try{
-        const {username, password, role} = req.body; 
-
-        const existingUser = await User.findOne({username});
-        if (existingUser) {
-            return res.status(400).json({message: "User already exists"});
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        const user = new User({
-            username, password: hashedPassword, role
-        }); 
-
-        await user.save(); 
-
-        res.json({message: "User Registered successfully"});
-    } catch (error) {
-        res.status(500).json({error: error.message});
-    }
-});
-
-router.post('/login', async(req, res) =>{
-    try{
-        const { username, password } = req.body; 
-
-        const user = await User.findOne({username}); 
-        if(!user){
-            return res.status(400).json({message: "Invalid password"});
-        }
-
-        const token = kwt.sign(
-            { id: user._id, role: user.role},
-            process.env.JWT_SECRET,
-            { expiresIn: "1h"}
-        );
-
-        res.json({token}); 
-    } catch (error){
-        res.status(500).json({error: error.message});
-    }
-});
+router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
